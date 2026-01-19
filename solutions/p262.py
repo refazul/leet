2d5108e2286dd4ca4b266a8ac1de21d75c912865
Refazul <refazul.refat@yahoo.com>
2026-01-19 13:44:18 +0600

    262, 601 formatting

--- solutions/p262.py
+++ solutions/p262.py
@@ -1,23 +1,103 @@
+# LeetCode 262. Trips and Users
+# https://leetcode.com/problems/trips-and-users/description/
+
+# Problem
+"""
+The cancellation rate is computed by dividing the number of canceled (by client or driver) requests with unbanned users by the total number of requests with unbanned users on that day.
+
+Write a solution to find the cancellation rate of requests with unbanned users (both client and driver must not be banned) each day between "2013-10-01" and "2013-10-03" with at least one trip. Round Cancellation Rate to two decimal points.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+Example 1:
+
+Input:
+Trips table:
++----+-----------+-----------+---------+---------------------+------------+
+| id | client_id | driver_id | city_id | status              | request_at |
++----+-----------+-----------+---------+---------------------+------------+
+| 1  | 1         | 10        | 1       | completed           | 2013-10-01 |
+| 2  | 2         | 11        | 1       | cancelled_by_driver | 2013-10-01 |
+| 3  | 3         | 12        | 6       | completed           | 2013-10-01 |
+| 4  | 4         | 13        | 6       | cancelled_by_client | 2013-10-01 |
+| 5  | 1         | 10        | 1       | completed           | 2013-10-02 |
+| 6  | 2         | 11        | 6       | completed           | 2013-10-02 |
+| 7  | 3         | 12        | 6       | completed           | 2013-10-02 |
+| 8  | 2         | 12        | 12      | completed           | 2013-10-03 |
+| 9  | 3         | 10        | 12      | completed           | 2013-10-03 |
+| 10 | 4         | 13        | 12      | cancelled_by_driver | 2013-10-03 |
++----+-----------+-----------+---------+---------------------+------------+
+Users table:
++----------+--------+--------+
+| users_id | banned | role   |
++----------+--------+--------+
+| 1        | No     | client |
+| 2        | Yes    | client |
+| 3        | No     | client |
+| 4        | No     | client |
+| 10       | No     | driver |
+| 11       | No     | driver |
+| 12       | No     | driver |
+| 13       | No     | driver |
++----------+--------+--------+
+Output:
++------------+-------------------+
+| Day        | Cancellation Rate |
++------------+-------------------+
+| 2013-10-01 | 0.33              |
+| 2013-10-02 | 0.00              |
+| 2013-10-03 | 0.50              |
++------------+-------------------+
+Explanation:
+On 2013-10-01:
+  - There were 4 requests in total, 2 of which were canceled.
+  - However, the request with Id=2 was made by a banned client (User_Id=2), so it is ignored in the calculation.
+  - Hence there are 3 unbanned requests in total, 1 of which was canceled.
+  - The Cancellation Rate is (1 / 3) = 0.33
+On 2013-10-02:
+  - There were 3 requests in total, 0 of which were canceled.
+  - The request with Id=6 was made by a banned client, so it is ignored.
+  - Hence there are 2 unbanned requests in total, 0 of which were canceled.
+  - The Cancellation Rate is (0 / 2) = 0.00
+On 2013-10-03:
+  - There were 3 requests in total, 1 of which was canceled.
+  - The request with Id=8 was made by a banned client, so it is ignored.
+  - Hence there are 2 unbanned request in total, 1 of which were canceled.
+  - The Cancellation Rate is (1 / 2) = 0.50
+"""
+
 import pandas as pd
 
 # Set pandas display options to show full DataFrame
-pd.set_option('display.max_rows', None)
-pd.set_option('display.max_columns', None)
-pd.set_option('display.width', None)
-pd.set_option('display.max_colwidth', None)
+pd.set_option("display.max_rows", None)
+pd.set_option("display.max_columns", None)
+pd.set_option("display.width", None)
+pd.set_option("display.max_colwidth", None)
+
 
 def trips_and_users(trips: pd.DataFrame, users: pd.DataFrame) -> pd.DataFrame:
-    table1 = trips.merge(users, how = 'left', left_on = 'client_id', right_on = 'users_id')
-    table2 = table1.merge(users, how = 'left', left_on = 'driver_id', right_on = 'users_id')
+    table1 = trips.merge(users, how="left", left_on="client_id", right_on="users_id")
+    table2 = table1.merge(users, how="left", left_on="driver_id", right_on="users_id")
 
-    clean_list = table2[(table2['banned_x'] == 'No') & (table2['banned_y'] == 'No') & (table2['request_at'] >= "2013-10-01") & (table2['request_at'] <= "2013-10-03")]
+    clean_list = table2[
+        (table2["banned_x"] == "No")
+        & (table2["banned_y"] == "No")
+        & (table2["request_at"] >= "2013-10-01")
+        & (table2["request_at"] <= "2013-10-03")
+    ]
 
-    agg = clean_list.groupby('request_at').agg(
-        day = ('request_at', 'first'),
-        total = ('status', 'size'),
-        completed = ('status', lambda x: (x == 'completed').sum())
+    agg = clean_list.groupby("request_at").agg(
+        day=("request_at", "first"),
+        total=("status", "size"),
+        completed=("status", lambda x: (x == "completed").sum()),
     )
-    agg['cancellation_rate'] = ((agg['total'] - agg['completed']) / agg['total']).round(2)
-    agg.rename(columns = {'cancellation_rate' : 'Cancellation Rate', 'day': 'Day'}, inplace = True)
-        
-    return agg[['Day', 'Cancellation Rate']]
+    agg["cancellation_rate"] = ((agg["total"] - agg["completed"]) / agg["total"]).round(
+        2
+    )
+    agg.rename(
+        columns={"cancellation_rate": "Cancellation Rate", "day": "Day"}, inplace=True
+    )
+
+    return agg[["Day", "Cancellation Rate"]]
